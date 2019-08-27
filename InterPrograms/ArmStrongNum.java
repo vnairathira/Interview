8973fa846be3e9936cd2b315e4a48f96f227af2d
vnairathira <44398747+vnairathira@users.noreply.github.com>
2019-08-28 01:01:46 +0530

    Add files via upload

---
+++ InterPrograms/ArmStrongNum.java
@@ -0,0 +1,20 @@
+package InterPrograms;
+
+public class ArmStrongNum {
+public static void main(String[] args) {
+	int n=153;
+	int a=n;
+	int cube=0;
+	while(n>0) {
+		int r=n%10;
+		cube = cube + (r*r*r);
+		n=n/10;
+	}
+	if(a==cube) {
+		System.out.println("armstrom");
+	}else {
+		System.out.println("not Armstrom");
+	}
+	}
+}
+
---
+++ InterPrograms/ArrayEqlsOrNot.java
@@ -0,0 +1,24 @@
+package InterPrograms;
+
+public class ArrayEqlsOrNot {
+	public static void main(String[] args) {
+		int[] a1= {1,2,3,5};
+		int[] a2= {1,2,8,5};
+		boolean val=true;
+		if(a1.length==a2.length) {
+		for(int i=0;i<a1.length;i++) {
+				if(a1[i]!=a2[i]) {
+					val=false;
+				}
+			}
+		}else {
+			val=false;
+		}
+		if(val) {
+			System.out.println("equal");
+		}else {
+			System.out.println("notequal");
+		}
+		
+	}
+}
